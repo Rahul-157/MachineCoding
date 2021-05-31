4dafb7c9e34a889b60e2975f9905a41010394e2a
rahul-157 <rahul157vanshi@gmail.com>
2021-05-31 20:19:18 +0530

    Snake and Ladder

---
+++ SnakeAndLadder/Main.java
@@ -0,0 +1,16 @@
+package SnakeAndLadder;
+
+
+/**
+ * javac SnakeAndLadder/Main.java -d output
+ * java -cp output/ SnakeAndLadder.Main
+ * https://workat.tech/machine-coding/practice/snake-and-ladder-problem-zgtac9lxwntg
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        SnakeAndLadder game = new SnakeAndLadder();
+        game.start();
+    }
+}
